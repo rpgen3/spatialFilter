6192aa69a1673f1e73e301414ceb2fdd4bdbad11
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-21 00:19:32 +0900

    Update util.mjs

--- mjs/util.mjs
+++ mjs/util.mjs
@@ -20,3 +20,10 @@ export const calcAny = ({k, width, height}) => { // 地味に必要な計算
           _height = height + __k; // 外周を埋めた高さ
     return {_k, __k, _width, _height};
 };
+export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+export const toI = (w, x, y) => x + y * w;
+export const toXY = (w, i) => {
+    const x = i % w,
+          y = i / w | 0;
+    return [x, y];
+};
