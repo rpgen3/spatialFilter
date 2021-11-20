f37d2e7ac8554897079b8500dcc4a9e692364fd9
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-20 23:09:33 +0900

    Update makeDataLuminance.mjs

--- mjs/makeDataLuminance.mjs
+++ mjs/makeDataLuminance.mjs
@@ -1,5 +1,5 @@
 const luminance = (r, g, b) => r * 0.298912 + g * 0.586611 + b * 0.114478;
-export const makeDataLuminance = async data => {
+export const makeDataLuminance = data => {
     const _data = new Uint8ClampedArray(data.length >> 2);
     for(const i of _data.keys()) {
         const _i = i << 2;
